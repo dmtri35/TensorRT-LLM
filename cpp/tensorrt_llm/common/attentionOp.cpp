4735061b43c6abfe8372281ab0fe013c40a4a5ea
Tri Dao <daominhtri0503@gmail.com>
2026-06-10 12:29:28 -0700

    [None][fix] disable multi-CTA for Helix MLA MTP microstep
    Signed-off-by: Tri Dao <daominhtri0503@gmail.com>
    (cherry picked from commit 1332d96e5853042b43e59798c3391bee7c3e5f28)
    Signed-off-by: Tri Dao <daominhtri0503@gmail.com>

--- cpp/tensorrt_llm/common/attentionOp.cpp
+++ cpp/tensorrt_llm/common/attentionOp.cpp
@@ -1264,6 +1264,8 @@ int AttentionOp::mlaGeneration(
             stepRunnerParams.seqLensKvPtr = stepKvLensPtr;
             stepRunnerParams.mMaxSeqLenQ = 1;
             stepRunnerParams.mSumOfSeqLensQ = batch_beam;
+            stepRunnerParams.mMultiCtasKvMode = false;
+            stepRunnerParams.mTileScheduler = TileScheduler::Persistent;
 
             for (int32_t step = 0; step < seqLenQ; ++step)
             {
